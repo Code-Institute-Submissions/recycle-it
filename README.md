4d856c5ebc7fd38bbfbb7e15d280607a7677bc55
corey-howard <coreyhoward@live.co.uk>
2020-09-26 18:33:06 +0000

    Fixed javascript functions so that the relevant item can only be dropped into the correct place. Added grabbable curserfor when clicking on items to be picked up and dragged/dropped

--- README.md
+++ README.md
@@ -13,3 +13,15 @@ https://fonts.google.com/
 Logo made with:
 
 https://www.wix.com/
+
+Bin images:
+
+https://www.hiclipart.com/free-transparent-background-png-clipart-dmwbx
+
+Background image:
+
+https://www.artstation.com/artwork/Y4l4q
+
+Drag and drop research:
+
+http://spolearninglab.com/curriculum/SEP/2015/unit_01/drag_and_drop.html
--- assets/js/level-one.js
+++ assets/js/level-one.js
@@ -1,21 +1,48 @@
-function allowDrop(ev) {
-  ev.preventDefault();
+// Items
+
+function drag1 (ev) {
+    ev.dataTransfer.setData("choice1", ev.target.id);
+    console.log("plastic");
 }
 
-function drag(ev) {
-  ev.dataTransfer.setData("text", ev.target.id);
-  console.log("plastic");
+function drag2 (ev) {
+    ev.dataTransfer.setData("choice2", ev.target.id);
+    console.log("paper");
 }
 
-if ($('#drag1').hasClass('plastic')) {
-   // Do something
-} else {
-   // Do something else
+function drag3 (ev) {
+    ev.dataTransfer.setData("choice3", ev.target.id);
+    console.log("glass");
 }
 
-function drop(ev) {
-  ev.preventDefault();
-  var data = ev.dataTransfer.getData("text");
-  ev.target.appendChild(document.getElementById(data));
-  playSound("/audio/plastic-crunch.mp3");
+// AllowDrop
+
+function allowDrop (ev) {
+    ev.preventDefault();
+}
+
+// SoundFile
+var audio = new Audio ('assets/audio/plastic-crunch.mp3');
+
+// Bins
+
+function drop1 (ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData ("choice1");
+    ev.target.appendChild(document.getElementById(data));
+    audio.play();
+}
+
+function drop2 (ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("choice2");
+    ev.target.appendChild(document.getElementById(data));
+    audio.play();
+}
+
+function drop3 (ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("choice3");
+    ev.target.appendChild(document.getElementById(data));
+    audio.play();
 }
